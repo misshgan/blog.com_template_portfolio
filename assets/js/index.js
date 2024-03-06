f3d39304df51e7c5aaf3c167b18e186539af9bb3
misshgan <medvedev3678@gmail.com>
2024-03-06 00:35:49 +0000

    main js file optimized

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,43 +1,61 @@
-// JavaScript files are compiled and minified during the build process to the assets/built folder. See available scripts in the package.json file.
-
-// Import CSS
 import "../css/index.css";
-
-import { handleHomeGalleryLightbox } from './photoSwipe';
-import { handlePostContentLightbox } from "./photoSwipe";
+import { handleHomeGalleryLightbox, handlePostContentLightbox } from './photoSwipe';
 import { handleHomeHeroSwiper } from "./swiper";
 
 function handleHeaderDropdown() {
     const target = document.querySelector('#header-nav-dropdown-target');
-    const dropdownBody = target.querySelector('ul')
-    if (!target) {return;}
 
-    if (dropdownBody.querySelector('li')) {
-        target.classList.remove('hidden')
-    }
+    // Error handling if target is not found
+    if (!target) return;
+
+    const dropdownBody = target.querySelector('ul');
+    const showDropdown = dropdownBody?.querySelector('li');
+
+    if (showDropdown) target.classList.remove('hidden');
 
     target.addEventListener('mouseover', () => {
-        dropdownBody.classList.add('active')
-    })
+        dropdownBody.classList.add('active');
+    });
+
     target.addEventListener('mouseout', () => {
-        dropdownBody.classList.remove('active')
-    })
+        dropdownBody.classList.remove('active');
+    });
 }
 
-handleHeaderDropdown();
+function handleMobileNav() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const targets = document.querySelectorAll('.js-mobile-menu-target');
 
-if (document.getElementById('home-hero-swiper')) {
-    handleHomeHeroSwiper();
+    // Error handling if targets are not found
+    if (!targets || !mobileMenu) return;
+
+    const toggleMobileMenu = () => {
+        mobileMenu.classList.toggle('active');
+    };
+
+    targets.forEach(target => {
+        target.addEventListener('click', toggleMobileMenu);
+    });
+
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 1024) {
+            mobileMenu.classList.remove('active');
+        }
+    });
 }
 
-if (document.getElementById('home-gallery')) {
-    handleHomeGalleryLightbox();
-}
+handleMobileNav(); // Handle mobile navigation menu
+handleHeaderDropdown(); // Handle header dropdown
 
-if (document.getElementById('post-content')) {
-    handlePostContentLightbox();
-}
+// Check if elements exist before invoking related functions
+const homeHeroSwiper = document.getElementById('home-hero-swiper');
+const homeGallery = document.getElementById('home-gallery');
+const postContent = document.getElementById('post-content');
 
-// Lazyload plugin
+if (homeHeroSwiper) handleHomeHeroSwiper();
+if (homeGallery) handleHomeGalleryLightbox();
+if (postContent) handlePostContentLightbox();
+
+// Import lazysizes plugin
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
